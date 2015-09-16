e23c0a21a32fcade5cdc56fa800d262296179a60
kennedy <kennedysgarage@gmail.com>
2015-09-16 10:50:34 -0400

    reverting back to the working map

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
 
   // This works, but it is not pulling in any data.
-  // $('#map').vectorMap({map: 'world_mill'});
+  $('#map').vectorMap({map: 'world_mill'});
 
   // This is from an example they provded.
-  $(function(){
-    $('#map').vectorMap({
-      map: 'world_mill',
-      series: {
-        regions: [{
-          values: gdpData,
-          scale: ['#C8EEFF', '#0071A4'],
-          normalizeFunction: 'polynomial'
-        }]
-      },
-      onRegionTipShow: function(e, el, code){
-        el.html(el.html()+' (GDP - '+gdpData[code]+')');
-      }
-    });
-  });
+  // $(function(){
+  //   $('#map').vectorMap({
+  //     map: 'world_mill',
+  //     series: {
+  //       regions: [{
+  //         values: gdpData,
+  //         scale: ['#C8EEFF', '#0071A4'],
+  //         normalizeFunction: 'polynomial'
+  //       }]
+  //     },
+  //     onRegionTipShow: function(e, el, code){
+  //       el.html(el.html()+' (GDP - '+gdpData[code]+')');
+  //     }
+  //   });
+  // });
 
 });
