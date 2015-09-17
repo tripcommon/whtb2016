686bc29e6c518083cb7e88fd3c629bf657efdbde
kennedy <kennedysgarage@gmail.com>
2015-09-17 13:49:15 -0400

    playing with the color

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
 
 });
 
-var basic = new Datamap({
-  element: document.getElementById("map")
-});
+
+var map = new Datamap({
+        element: document.getElementById('map'),
+        fills: {
+            defaultFill: 'rgba(66,70,81,0.9)' //any hex, color name or rgb/rgba value
+        }
+    });
